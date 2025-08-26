ed723c5abf83f4b1d7c3223d93b21f062450d583
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-26 09:05:21 +0600

    soze of

---
+++ dynamic_memory.c
@@ -0,0 +1,32 @@
+// ================================================
+//               * Language: C
+//               * Topic: Dynamic Memory Allocation
+// ===============================================
+
+//            -----  📝📝 --
+
+#include <stdio.h>
+int main(){
+    printf("Size of  int %d\n" ,sizeof(int));
+    printf("Size of float %d\n" , sizeof(float));
+    printf("Size of char %d\n" , sizeof(char));
+
+    return  0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 👉👉  🔹🔹  Question  
+// 👉👉  🔹🔹  Question
+// 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
+// 💥 ⚡ 🔥 ✨  📌  => Notes / Markers📌📍🎯📝
