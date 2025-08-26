30cacf0a7904569b71ffd0dd6690d5e08b6d7438
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-27 00:31:32 +0600

    solve 4th problem od 'Dynamic memory 'in C

--- solve_problem.c
+++ solve_problem.c
@@ -28,35 +28,70 @@
 // 👉👉  🔹🔹  Question  2️⃣ WAP to allocate memory  of size n , where entered by the user 
 
 
-#include <stdio.h>
-#include <stdlib.h>
-float main(){
-  int   *ptr ;
-  int n;
-  printf("Enter n  : ");
-   scanf("%d" , &n);
+// #include <stdio.h>
+// #include <stdlib.h>
+// float main(){
+//   int   *ptr ;
+//   int n;
+//   printf("Enter n  : ");
+//    scanf("%d" , &n);
 
-   ptr = (int *) calloc(n , sizeof(int));
+//    ptr = (int *) calloc(n , sizeof(int));
 
-   for (int  i = 0; i <n; i++)
-   {
-   printf("%d\n" , ptr[i]);
-   }
+//    for (int  i = 0; i <n; i++)
+//    {
+//    printf("%d\n" , ptr[i]);
+//    }
    
 
 
+//     return 0;
+// }
+
+
+
+
+// 👉👉 🔹🔹 Question 3️⃣  free the memoru allocated  with calloc
+
+
+
+// 👉👉 🔹🔹 Question  4️⃣   Allocated  memory for 5  numbers  then dynamically  increase it  to 8 numbers
+
+
+#include <stdio.h>
+#include <stdlib.h>
+float main(){
+    int *ptr;
+    ptr = (int *) calloc(5, sizeof(int));
+
+    printf("Enter 5 Numbers");
+    for (int i = 0; i <5; i++)
+    {
+        scanf("%d" , &ptr[i]);
+        
+    }
+
+    ptr =   realloc(ptr ,8); 
+    printf("Enter 8  Numbers : ");
+    for(int i = 0 ; i< 8 ; i++){
+        scanf("%d" , &ptr[i]);
+    } 
+    
+    // print
+    for (int i = 0; i < 8; i++)
+    {
+       printf("Number %d ,is  %d\n" , i , ptr[i]);
+    }
+    
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
+      
+// #include <stdio.h>
+// #include <stdlib.h>
+// float main(){
+//     return 0;
+// }
 
 
 
